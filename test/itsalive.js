8601d3a0d657f713f3ec840e0f4e50cd8918aff3
Meredith Roman <meredith.c.roman@gmail.com>
2016-06-22 15:36:07 -0400

    added some tests yay

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,12 +1,29 @@
 var chai = require('chai');
+var spies = require('chai-spies');
 var expect = chai.expect;
 
-describe('Testing suit capabilities', function(){
-	it('works the way math works', function(){
+chai.use(spies);
+
+describe('Testing suit capabilities', function (){
+	it('works the way math works', function (){
 		expect(2 + 2).to.equal(4);
 	});
-	it('checks that setTimeout works', function(done){
-		
+	it('checks that setTimeout works', function (done){
+		var start = new Date();
+		setTimeout(function () {
+			var startAgain = new Date();
+			var elapsed = startAgain - start;
+			expect(elapsed).to.be.closeTo(1000, 5);
+			done();
+		}, 1000);
+	});
+	it('makes sure that forEach works like forEach works', function () {
+		var someArrayOfNums = [1, 2, 3, 4, 5, 6];
+		function uselessFunction () {
+			console.log("it really doesn't matter what I do in here");
+		}
+		var uselessSpy = chai.spy(uselessFunction);
+		someArrayOfNums.forEach(uselessSpy);
+		expect(uselessSpy).to.have.been.called.exactly(someArrayOfNums.length);
 	});
 });
-
